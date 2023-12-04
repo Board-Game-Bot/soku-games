3c799816199943522fed663b6c00e1f94d926362
andrew <1062708758@qq.com>
2023-12-05 01:49:03 +0800

    feat: show how to use.

--- web-app/src/utils.ts
+++ web-app/src/utils.ts
@@ -1,10 +1,11 @@
 import { createSignal } from 'solid-js';
 
 export const downloadGame = (npmPackage: string, version: string, stuff: string, onLoad: () => void) => {
-  if (document.querySelector(`script#${npmPackage}`)) return ;
+  if (document.querySelector(`script#${npmPackage}-${version}`)) return ;
 
   const dom = document.createElement('script');
   dom.type = 'module';
+  dom.id = `${npmPackage}-${version}`;
   dom.src = `https://cdn.jsdelivr.net/npm/${npmPackage}@${version}/dist/${stuff}/index.js`;
   document.body.append(dom);
   dom.onload = onLoad;
