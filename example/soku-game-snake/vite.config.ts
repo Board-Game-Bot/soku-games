9b77d1fa71933ad8c140d7683e2cca1a700e95bf
andrew <1062708758@qq.com>
2023-12-31 16:50:22 +0800

    chore: use @soku-games/vite-plugin

--- example/soku-game-snake/vite.config.ts
+++ example/soku-game-snake/vite.config.ts
@@ -1,32 +1,7 @@
-import { defineConfig, PluginOption, UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sokuGames from '@soku-games/vite-plugin';
 
-export default defineConfig(({ mode }) => {
-  const plugins: PluginOption[] = [
-    react(),
-  ];
-  function makeConfig(name: string): UserConfig {
-    return {
-      plugins,
-      build: {
-        outDir: `./dist-${name}`,
-        rollupOptions: {
-          input: `./src/${name}.ts`,
-          output: [{
-            name: 'index',
-            entryFileNames: 'index.iife.js',
-            format: 'iife',
-          }, {
-            entryFileNames: 'index.cjs.js',
-            format: 'cjs',
-          }, {
-            entryFileNames: 'index.esm.js',
-            format: 'esm',
-          }],
-          external: ['@soku-games/core'],
-        },
-      },
-    };
-  }
-  return makeConfig(mode);
+export default defineConfig({
+  plugins: [react(), sokuGames()],
 });
